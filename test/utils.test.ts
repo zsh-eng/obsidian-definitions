5b6d3f8f5445dda261310f2a8615da4f19b42868
sheng <chengzhisheng2001@gmail.com>
2023-07-05 23:37:43 +0800

    feat: Add replacer function
    This commit adds a function to replace text that is outside of square
    brackets.

---
+++ test/utils.test.ts
@@ -0,0 +1,74 @@
+import { replaceExceptBrackets } from "../utils";
+
+describe("replaceExceptBrackets", () => {
+	test("empty string is not replaced", () => {
+		const from = "hello";
+		const to = "goodbye";
+		const content = "";
+		expect(replaceExceptBrackets(from, to, content)).toBe("");
+	});
+
+	test("replaces a string outside brackets", () => {
+		const from = "world";
+		const to = "Universe";
+		const content = "Hello, world! [Another world]";
+		const expected = "Hello, Universe! [Another world]";
+		const result = replaceExceptBrackets(from, to, content);
+		expect(result).toBe(expected);
+	});
+
+	test("does not replace a string inside brackets", () => {
+		const from = "world";
+		const to = "Universe";
+		const content = "Hello, [world]!";
+		const expected = "Hello, [world]!";
+		const result = replaceExceptBrackets(from, to, content);
+		expect(result).toBe(expected);
+	});
+
+	test("replaces multiple instances outside brackets", () => {
+		const from = "world";
+		const to = "Universe";
+		const content = "Hello, world! Another world! [Yet another world]";
+		const expected =
+			"Hello, Universe! Another Universe! [Yet another world]";
+		const result = replaceExceptBrackets(from, to, content);
+		expect(result).toBe(expected);
+	});
+
+	test("handles special characters in the string to replace", () => {
+		const from = "(special)";
+		const to = "replacement";
+		const content = "This is a (special) case";
+		const expected = "This is a replacement case";
+		const result = replaceExceptBrackets(from, to, content);
+		expect(result).toBe(expected);
+	});
+
+	test("handles special characters in the replacement string", () => {
+		const from = "world";
+		const to = "[Universe]";
+		const content = "Hello, world!";
+		const expected = "Hello, [Universe]!";
+		const result = replaceExceptBrackets(from, to, content);
+		expect(result).toBe(expected);
+	});
+
+	test("handles nested brackets", () => {
+		const from = "hello";
+		const to = "world";
+		const content = "[[sometext]hello]";
+		const expected = "[[sometext]hello]";
+		const result = replaceExceptBrackets(from, to, content);
+		expect(result).toBe(expected);
+	});
+
+	test("handles adjacent brackets", () => {
+		const from = "hello";
+		const to = "world";
+		const content = "[]hello[]";
+		const expected = "[]world[]";
+		const result = replaceExceptBrackets(from, to, content);
+		expect(result).toBe(expected);
+	});
+});
---
+++ utils.ts
@@ -0,0 +1,36 @@
+import { EditorPosition } from "obsidian";
+
+export const DOCUMENT_START: EditorPosition = {
+	line: 0,
+	ch: 0,
+};
+
+export const DOCUMENT_END: EditorPosition = {
+	line: Infinity,
+	ch: Infinity,
+};
+
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
+function escapeRegExp(str: string): string {
+	return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+// Replaces all instances of a string except those inside brackets.
+export function replaceExceptBrackets(
+	from: string,
+	to: string,
+	content: string
+): string {
+	/* (?!\\[ Uses negative lookahead to check for opening brackets
+		[^\\]]* Uses a negated character class to match zero or more occurrences
+		of any character except ]
+	*/
+	const escapedFrom = escapeRegExp(from);
+	const pattern = new RegExp(
+		`(?!\\[[^\\]]*)${escapedFrom}(?![^\\[]*\\])`,
+		"g"
+	);
+	const replaced = content.replace(pattern, to);
+
+	return replaced;
+}
