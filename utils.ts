ca0b571c45d76acd2a05c594012a99bf0f260edb
sheng <chengzhisheng2001@gmail.com>
2023-07-18 10:18:44 +0800

    Add caching of AST
    Caching of AST implementation referenced from obsidian-linter plugin:
    https://github.com/platers/obsidian-linter/blob/master/src/utils/mdast.ts#L56

--- utils.ts
+++ utils.ts
@@ -6,6 +6,8 @@ import { fromMarkdown } from "mdast-util-from-markdown";
 import { syntax } from "micromark-extension-wiki-link";
 // @ts-ignore
 import * as wikiLink from "mdast-util-wiki-link";
+import QuickLRU from "quick-lru";
+import { Root } from "mdast-util-from-markdown/lib";
 
 export type Definition = {
 	filename: string;
@@ -85,13 +87,52 @@ export function replaceDefinitions(
 	return out.join("");
 }
 
-// Returns the positions of all text (i.e. non codeblocks, wikilinks, etc.) in the markdown string
-export function getPositions(content: string): Position[] {
+// Hash function from https://stackoverflow.com/a/52171480/8353749
+export function hashString(str: string, seed: number = 0): number {
+	let h1 = 0xdeadbeef ^ seed;
+	let h2 = 0x41c6ce57 ^ seed;
+	for (let i = 0, ch; i < str.length; i++) {
+		ch = str.charCodeAt(i);
+		h1 = Math.imul(h1 ^ ch, 2654435761);
+		h2 = Math.imul(h2 ^ ch, 1597334677);
+	}
+
+	h1 =
+		Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^
+		Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+	h2 =
+		Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^
+		Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+	return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+}
+
+// Cache for storing the ASTs of markdown strings
+const LRU = new QuickLRU<number, Root>({ maxSize: 200 });
+
+// Parses the markdown string into an mdast
+function stringToAST(content: string): Root {
+	const textHash = hashString(content);
+	if (LRU.has(textHash)) {
+		const ast = LRU.get(textHash);
+
+		if (ast !== undefined) {
+			return ast;
+		}
+	}
+
 	const ast = fromMarkdown(content, {
 		extensions: [syntax({ aliasDivider: "|" })],
 		mdastExtensions: [wikiLink.fromMarkdown()], // Parse Obsidian [[wikiLinks]]
 	});
 
+	LRU.set(textHash, ast);
+	return ast;
+}
+
+// Returns the positions of all text (i.e. non codeblocks, wikilinks, etc.) in the markdown string
+export function getPositions(content: string): Position[] {
+	const ast = stringToAST(content);
 	const positions: Position[] = [];
 
 	visit(ast, "text", (node, index, parent) => {
